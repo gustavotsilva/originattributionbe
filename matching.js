fb8821d17a32c5e2e092619cc680fb3c20bca65f
gustavo.silva <gustavo.silva@useorigin.com>
2024-07-12 00:06:55 +0000

    change minutes

--- matching.js
+++ matching.js
@@ -10,11 +10,11 @@ const getSecondsBetweenTimestamps = (timestamp1, timestamp2) =>
  
  export function getMatchingConfidence(pulse_db, pulse_client){
    const secondsInBetween = getSecondsBetweenTimestamps(pulse_client.timestampUTC, pulse_db.timestampUTC);
-         if(secondsInBetween <= 90)   return Number(0.98 + deviceMatchingScore(pulse_db, pulse_client, 0.01)).toFixed(2);  //max: 100%
-   else  if(secondsInBetween <= 120)  return Number(0.95 + deviceMatchingScore(pulse_db, pulse_client, 0.01)).toFixed(2);  //max: 97%
-   else  if(secondsInBetween <= 300)  return Number(0.90 + deviceMatchingScore(pulse_db, pulse_client, 0.025)).toFixed(2); //max: 95%
-   else  if(secondsInBetween <= 600)  return Number(0.80 + deviceMatchingScore(pulse_db, pulse_client, 0.05)).toFixed(2);  //max: 90%
-   else  if(secondsInBetween <= 900)  return Number(0.70 + deviceMatchingScore(pulse_db, pulse_client, 0.075)).toFixed(2); //max: 85%
-   else  if(secondsInBetween <= 1200) return Number(0.50 + deviceMatchingScore(pulse_db, pulse_client, 0.1)).toFixed(2);   //max: 70%
+         if(secondsInBetween <= 180)  return Number(0.98 + deviceMatchingScore(pulse_db, pulse_client, 0.01)).toFixed(2);  //max: 100%
+   else  if(secondsInBetween <= 300)  return Number(0.95 + deviceMatchingScore(pulse_db, pulse_client, 0.01)).toFixed(2);  //max: 97%
+   else  if(secondsInBetween <= 600)  return Number(0.90 + deviceMatchingScore(pulse_db, pulse_client, 0.025)).toFixed(2); //max: 95%
+   else  if(secondsInBetween <= 900)  return Number(0.80 + deviceMatchingScore(pulse_db, pulse_client, 0.05)).toFixed(2);  //max: 90%
+   else  if(secondsInBetween <= 1200) return Number(0.70 + deviceMatchingScore(pulse_db, pulse_client, 0.075)).toFixed(2); //max: 85%
+   else  if(secondsInBetween <= 1800) return Number(0.50 + deviceMatchingScore(pulse_db, pulse_client, 0.1)).toFixed(2);   //max: 70%
    return 0;
  }
